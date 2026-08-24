9ece8ae07d94a352dc85d13231899f66bb38d3b3
Wan S. Ayub <w.s.ayub@gmail.com>
2026-08-24 11:22:29 +0800

    user controller

---
+++ app/controllers/user_controller.py
@@ -0,0 +1,13 @@
+class UserController:
+    # def _init_(self):
+    #     pass
+
+    @staticmethod
+    def authenticate(params={}):
+        # validate username, password
+        # send to auth service
+        # auth service generate jwt token
+        # jwt token save in authorized header
+        # return token, user data
+        # consider: session, exist generated token
+        pass
